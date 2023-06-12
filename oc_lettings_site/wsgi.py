8977ee24dc751b977bbc6ac42a9924e157679081
HaroldHaldemann <harold.haldemann@sfr.fr>
2023-06-12 14:05:49 +0200

    revert: cancel static

--- oc_lettings_site/wsgi.py
+++ oc_lettings_site/wsgi.py
@@ -2,9 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from dj_static import Cling
-
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oc_lettings_site.settings')
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
